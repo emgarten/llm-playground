3bb57bff32f41720f2e5fad985bbd437420d24d7
Justin Emgarten <juste@microsoft.com>
2025-01-24 22:25:45 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # llm-playground
-Hands with local LLMs
+
+Hands on with LLMs
+
+```bash
+
+# Start ollama and webui
+docker compose up -d
+
+# Download models
+./pull-models.sh
+
+# Open UX
+http://localhost:3000
+
+```
--- pull-models.sh
+++ pull-models.sh
@@ -2,7 +2,7 @@
 
 # https://ollama.com/library/deepseek-r1
 docker exec -it ollama ollama pull deepseek-r1:1.5b
-docker exec -it ollama ollama pull deepseek-r1:7b
+# docker exec -it ollama ollama pull deepseek-r1:7b
 # docker exec -it ollama ollama pull deepseek-r1:14b
 
 # https://ollama.com/library/phi3
